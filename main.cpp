5ff22c8851511c423ba65467dfbb44a03242c9aa
wfqLab <63701501+wfqLab@users.noreply.github.com>
2023-07-02 14:37:59 +0800

    Update main.cpp
    argc parameter show

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include<iostream>
 using namespace std;
-int main()
+int main(int argc, char *argv[])
 {
-	cout<<"hello world"<<endl;
-
+	
+	cout<<"hello world\n"<<endl;
+	cout<<"display number: "<<argc<<endl;
+	for(int count=0; count<argc; count++)
+	{
+		cout<<"argv["<<count<<"]: "<<argv[]<<endl;
+	}
 }
